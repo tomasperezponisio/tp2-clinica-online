5cbc9fe61d0ba798b859ba7720f497b8154ebc35
tperezpo <tomasalejandro.perezponisio@emeal.nttdata.com>
2024-10-30 10:17:32 -0300

    Primer commit

---
+++ src/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  apiKey: "AIzaSyCvjCprlKMqxHBEbPLMQMe7j6bUiYs4Y4k",
+  authDomain: "primer-parcial-labo-4-318de.firebaseapp.com",
+  projectId: "primer-parcial-labo-4-318de",
+  storageBucket: "primer-parcial-labo-4-318de.appspot.com",
+  messagingSenderId: "355081823358",
+  appId: "1:355081823358:web:789319df92a74ef2ece42f"
+}

473a9af1754f98a9ebafd75831fa081f6a9e41dc
tomasperezponisio <elunicotomas@gmail.com>
2024-11-26 14:32:37 -0300

    sprint 4 - cuarto agregado sprint 3, animaciones

--- src/app/animations/animations.ts
+++ src/app/animations/animations.ts
@@ -1,8 +1,32 @@
-import { trigger, group, style, animate, transition, query, animateChild,  } from '@angular/animations';
+import {trigger, group, style, animate, transition, query, animateChild,} from '@angular/animations';
 
 export const slideInAnimation =
   trigger('routeAnimations', [
     transition('login <=> register, login <=> home, register <=> home', [
+      style({position: 'relative'}),
+      query(':enter, :leave', [
+        style({
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%'
+        })
+      ], {optional: true}),
+      query(':enter', [
+        style({left: '-100%'})
+      ], {optional: true}),
+      group([
+        query(':leave', [
+          animate('300ms ease-out', style({left: '100%'}))
+        ], {optional: true}),
+        query(':enter', [
+          animate('300ms ease-out', style({left: '0%'}))
+        ], {optional: true}),
+        query('@*', animateChild(), {optional: true})
+      ]),
+    ]),
+    transition('* <=> *', [
+      style({position: 'relative'}),
       query(':enter, :leave', [
         style({
           position: 'absolute',
@@ -13,30 +37,7 @@ export const slideInAnimation =
         }),
       ]),
       query(':enter', [
-        animate('300ms ease', style({ opacity: 1, transform: 'scale(1) translateY(0)' })),
+        animate('200ms ease', style({opacity: 1, transform: 'scale(1) translateY(0)'})),
       ])
-    ]),
-    transition('* <=> *', [
-      style({ position: 'relative' }),
-      query(':enter, :leave', [
-        style({
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          width: '100%'
-        })
-      ], { optional: true }),
-      query(':enter', [
-        style({ left: '-100%'})
-      ], { optional: true }),
-      group([
-        query(':leave', [
-          animate('300ms ease-out', style({ left: '100%'}))
-        ], { optional: true }),
-        query(':enter', [
-          animate('300ms ease-out', style({ left: '0%'}))
-        ], { optional: true }),
-        query('@*', animateChild(), { optional: true })
-      ]),
     ])
   ]);
